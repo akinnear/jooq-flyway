71de8a8a8908ede07b4fae5918bd22c5edd2323e
Adam Kinnear <akinnear@gmail.com>
2019-12-14 08:24:01 -0800

    Initial

---
+++ src/test/java/com/kinnear/dbtest/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kinnear.dbtest.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
